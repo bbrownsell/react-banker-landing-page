ae6442d0390bc9ed81b835bf9283d7e665526578
bbrownsell <ben.brownsell@hotmail.co.uk>
2021-05-25 21:03:00 +0100

    first commit

---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import Pricing from '../../Pricing';
+import {homeObjOne} from './Data';
+
+const Services = () => {
+    return (
+        <>
+            <Pricing/>
+            <HeroSection {...homeObjOne}/>
+            
+        </>
+    )
+}
+
+export default Services
